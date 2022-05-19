2219dc957a62de9a52287ed57478101d5cedc5df
Sumed Chaus <sumedchaus77@gmail.com>
2022-05-19 12:17:22 +0530

    part 1

--- app/src/main/java/com/cs/daggerhiltphilip/MainActivity.kt
+++ app/src/main/java/com/cs/daggerhiltphilip/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var testString: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
